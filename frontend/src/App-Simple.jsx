5604a49f2b0df7458ccdc38faf9d68bc2176120d
VICTOR <omeifevictor001@gmail.com>
2026-04-17 12:34:07 +0100

    Test simple app

---
+++ frontend/src/App-Simple.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>EduFlow is working!</h1>
+      <p>If you see this, the deployment is fine.</p>
+      <a href="/login">Go to Login</a>
+    </div>
+  );
+}
+
+export default App;
