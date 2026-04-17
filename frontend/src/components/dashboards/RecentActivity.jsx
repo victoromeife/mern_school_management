fbb94a54d850600f659709caf58b6b22637bcf85
VICTOR <omeifevictor001@gmail.com>
2026-04-17 11:58:31 +0100

    Add missing Card import to RecentActivity

--- frontend/src/components/dashboards/RecentActivity.jsx
+++ frontend/src/components/dashboards/RecentActivity.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
+import Card from '../ui/Card';
 import {
     UserPlusIcon,
     DocumentTextIcon,
